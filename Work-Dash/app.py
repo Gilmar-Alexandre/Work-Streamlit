cc169ca7938121aea5e1e174fb3e4acd5b4f435f
Gilmar Alexandre <gilmar.alexandre.sc@outlook.com>
2024-10-27 23:53:32 -0300

    dados

--- Work-Dash/app.py
+++ Work-Dash/app.py
@@ -1,15 +1,10 @@
 import pandas as pd
 import streamlit as st
 import pandas as pd
-import time
 
-@st.cache_data
-def load_data():
-    df = pd.read_excel('dados.xlsx')
-    time.sleep(5)
-    return df
-df = load_data()
-st.session_state["df"] = df
+
+df = pd.read_excel('dados.xlsx')
+df
 
 
 
